b2a23c097302dae597c24049c03438da25b898e4
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2025-08-31 11:45:52 +0900

    add:claude actions

--- react-basis/src/index.js
+++ react-basis/src/index.js
@@ -4,16 +4,6 @@ import { createRoot } from "react-dom/client"
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
-// アロー関数で記載する
-const App = () => {
-  return (
-    <>
-      <h1>こんにちは!</h1>
-      <p>お元気ですか？</p>
-    </>
-  ); 
-}
-
 // ワンラインナーでも記述可
 // const App = () => <h1>こんにちは!</h1>;
 
