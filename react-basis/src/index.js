41dca1892f74b14aabff16dbcdcfd2b9692b54a3
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2025-08-31 11:58:06 +0900

    add:コンポーネント化

--- react-basis/src/index.js
+++ react-basis/src/index.js
@@ -1,5 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client"
+// 拡張子は省略できる。定義している名前と同じ名前でインポートする
+import { App } from "./App";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
